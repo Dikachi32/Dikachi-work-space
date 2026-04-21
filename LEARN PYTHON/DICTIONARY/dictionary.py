c5395d0772883babc3265b56293638b875c1af5f
Dikachi32 <francisonyedikachiagwu@gmail.com>
2026-04-21 00:48:40 -0700

    practicing dictionary

---
+++ LEARN PYTHON/DICTIONARY/dictionary.py
@@ -0,0 +1,18 @@
+##. A dictionary stores data in key → value pairs. Think of it like a real dictionary:
+# student = {
+#     "name": "David",
+#     "age": 22,
+#     "score": 90
+# }
+# # Word = key  "name" → key
+# # Meaning = value "David" → value
+# print(student["name"])
+
+# Create a dictionary called car with: brand, year, price. Then print it.
+car = {
+    "brand": "Benz",
+    "year": 2025,
+    "price": 20000
+}
+print(car)
+print(car["price"])
